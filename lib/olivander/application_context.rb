04b81561d74cac6fb0bcff60c65e1c9fcbe13629
Eric Dennis <eric.dennis@5and2northwest.com>
2024-02-21 09:11:48 -0800

    0.2.0.6 login logo

--- lib/olivander/application_context.rb
+++ lib/olivander/application_context.rb
@@ -1,10 +1,11 @@
 module Olivander
   class ApplicationContext
-    attr_accessor :name, :logo, :company, :menu_items, :route_builder, :sign_out_path, :sidebar_background_class
+    attr_accessor :name, :logo, :login_logo, :company, :menu_items, :route_builder, :sign_out_path, :sidebar_background_class
 
     def initialize(**kwargs)
       self.name = kwargs[:name] || ENV['OLIVANDER_APP_NAME'] || 'Application Name'
       self.logo = kwargs[:logo] || Logo.new(url: kwargs[:logo_url], alt: kwargs[:logo_alt])
+      self.login_logo = kwargs[:login_logo] || LoginLogo.new(url: kwargs[:login_logo_url], alt: kwargs[:login_logo_alt])
       self.company = kwargs[:company] || Company.new(name: kwargs[:company_name], url: kwargs[:company_url])
       self.sign_out_path = kwargs[:sign_out_path] || '/sign_out'
       self.menu_items = kwargs[:menu_items] || []
@@ -38,6 +39,15 @@ module Olivander
       def initialize(**kwargs)
         self.url = kwargs[:url] || ENV['OLIVANDER_LOGO_URL'] || '/images/olivander_logo.png'
         self.alt = kwargs[:alt] || ENV['OLIVANDER_LOGO_ALT'] || 'Logo Image'
+      end
+    end
+
+    class LoginLogo
+      attr_accessor :url, :alt
+
+      def initialize(**kwargs)
+        self.url = kwargs[:url] || ENV['OLIVANDER_LOGIN_LOGO_URL'] || '/images/olivander_login_logo.png'
+        self.alt = kwargs[:alt] || ENV['OLIVANDER_LOGIN_LOGO_ALT'] || 'Login Logo Image'
       end
     end
 
