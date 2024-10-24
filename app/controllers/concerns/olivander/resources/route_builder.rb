1b39e2879fbd8984503538467860160bd1a0cb67
Eric Dennis <eric.dennis@5and2northwest.com>
2024-10-24 10:54:28 -0700

    0.2.0.37 enhance route builder args

--- app/controllers/concerns/olivander/resources/route_builder.rb
+++ app/controllers/concerns/olivander/resources/route_builder.rb
@@ -3,39 +3,41 @@ module Olivander
     class ResourceAction
       attr_accessor :sym, :action, :verb, :confirm, :turbo_frame, :collection,
                     :controller, :crud_action, :show_in_form, :show_in_datatable,
-                    :no_route, :path_helper, :confirm_with, :primary
+                    :no_route, :path_helper, :confirm_with, :primary,
+                    :turbo, :html_attributes
 
-      def initialize(sym, action: nil, controller: nil, verb: :get, confirm: false,
-                     turbo_frame: nil, collection: false, crud_action: false,
-                     show_in_form: true, show_in_datatable: true, no_route: false,
-                     path_helper: nil, confirm_with: nil, primary: nil)
+      def initialize(sym, **kwargs)
         self.sym = sym
-        self.action = action || sym
-        self.controller = controller
-        self.verb = verb
-        self.confirm = confirm
-        self.turbo_frame = turbo_frame
-        self.collection = collection
-        self.crud_action = crud_action
-        self.show_in_form = show_in_form
-        self.show_in_datatable = show_in_datatable
-        self.no_route = no_route
-        self.path_helper = path_helper
-        self.confirm_with = confirm_with
-        self.primary = primary || crud_action
+        self.action = kwargs[:action] || sym
+        self.controller = kwargs[:controller]
+        self.verb = kwargs[:verb] || :get
+        self.confirm = kwargs[:confirm] || false
+        self.turbo_frame = kwargs[:turbo_frame]
+        self.turbo = kwargs[:turbo] == true ? true : !turbo_frame.blank?
+        self.collection = kwargs[:collection] || false
+        self.crud_action = kwargs[:crud_action] || false
+        self.show_in_form = kwargs[:show_in_form] || true
+        self.show_in_datatable = kwargs[:show_in_datatable] || true
+        self.no_route = kwargs[:no_route] || false
+        self.path_helper = kwargs[:path_helper]
+        self.confirm_with = kwargs[:confirm_with]
+        self.primary = kwargs[:primary] || crud_action
+        self.html_attributes = kwargs[:html_attributes]
       end
 
       def args_hash(options = nil)
         {}.tap do |h|
-          h.merge!(method: verb) if turbo_frame.blank?
+          h.merge!(method: verb) unless turbo
           h.merge!(data: data_hash)
+          h.merge!(html_attributes) if html_attributes
           h.merge!(options) if options.present?
         end
       end
 
       def data_hash
         {}.tap do |h|
-          h.merge!(turbo: true, turbo_method: verb, turbo_frame: turbo_frame) if turbo_frame.present?
+          h.merge!(turbo: true, turbo_method: verb) if turbo
+          h.merge!(turbo_frame: turbo_frame) if turbo_frame.present?
 
           message = confirmation_message
           h.merge!(confirm_key => message) unless message.blank?
@@ -43,7 +45,7 @@ module Olivander
       end
 
       def confirm_key
-        turbo_frame.present? ? :turbo_confirm : :confirm
+        turbo ? :turbo_confirm : :confirm
       end
 
       def confirmation_message
@@ -63,7 +65,9 @@ module Olivander
         self.actions = []
         %i[index new create edit show update destroy].each do |ca|
           next unless crud_actions.include?(ca)
-          actions << ResourceAction.new(ca, controller: model, verb: resolve_crud_action_verb(ca), collection: resolve_crud_action_collection(ca), crud_action: true)
+
+          actions << ResourceAction.new(ca, controller: model, verb: resolve_crud_action_verb(ca),
+                                            collection: resolve_crud_action_collection(ca), crud_action: true)
         end
       end
 
@@ -85,29 +89,29 @@ module Olivander
       end
 
       def crud_actions
-        actions.select{ |x| x.crud_action }
+        actions.select { |x| x.crud_action }
       end
 
       def member_actions
-        actions.select{ |x| !x.collection }
+        actions.select { |x| !x.collection }
       end
 
       def collection_actions
-        actions.select{ |x| x.collection }
+        actions.select { |x| x.collection }
       end
 
       def datatable_bulk_actions
-        collection_actions.select{ |x| x.show_in_datatable && !x.crud_action }
+        collection_actions.select { |x| x.show_in_datatable && !x.crud_action }
       end
 
       def unpersisted_crud_actions
         allowed = %i[index new]
-        crud_actions.select{ |x| allowed.include?(x.sym) }
+        crud_actions.select { |x| allowed.include?(x.sym) }
       end
 
       def persisted_crud_actions
         allowed = %i[show edit destroy]
-        crud_actions.select{ |x| allowed.include?(x.sym) }
+        crud_actions.select { |x| allowed.include?(x.sym) }
       end
     end
 
@@ -123,25 +127,18 @@ module Olivander
       end
 
       class_methods do
-        def resource(model, only: DEFAULT_CRUD_ACTIONS, except: [], namespaces: [], &block)
+        def resource(model, only: DEFAULT_CRUD_ACTIONS, except: [], namespaces: [])
           self.current_resource = RoutedResource.new(model, namespaces, DEFAULT_CRUD_ACTIONS & (only - except))
           yield if block_given?
           resources[model] = current_resource
           self.current_resource = nil
         end
 
-        def action(sym, verb: :get, confirm: false, turbo_frame: nil, collection: false, show_in_datatable: true,
-                   show_in_form: true, no_route: false, controller: nil, action: nil, path_helper: nil,
-                   confirm_with: nil, primary: nil
-                  )
+        def action(sym, **kwargs)
           raise 'Must be invoked in a resource block' unless current_resource.present?
 
           controller ||= current_resource.model
-          current_resource.actions << ResourceAction.new(
-            sym, action: action, controller: controller, verb: verb, confirm: confirm, turbo_frame: turbo_frame, collection: collection,
-            show_in_datatable: show_in_datatable, show_in_form: show_in_form, no_route: no_route, path_helper: path_helper,
-            confirm_with: confirm_with, primary: primary
-          )
+          current_resource.actions << ResourceAction.new(sym, **kwargs)
         end
 
         def build_routes(mapper)
@@ -185,13 +182,16 @@ module Olivander
                 end
               end
 
-              mapper.new do
-                mapper.get :new
-              end if r.collection_actions.select{ |x| x.action == :new }.size.positive?
+              if r.collection_actions.select { |x| x.action == :new }.size.positive?
+                mapper.new do
+                  mapper.get :new
+                end
+              end
 
               mapper.member do
                 r.member_actions.each do |ma|
                   next if ma.no_route
+
                   if ma.confirm
                     mapper.get ma.action, action: "confirm_#{ma.action}"
                     set_controller_and_helper(ma)
--- lib/olivander/version.rb
+++ lib/olivander/version.rb
@@ -2,5 +2,5 @@
 
 # needed for gem
 module Olivander
-  VERSION = '0.2.0.36'
+  VERSION = '0.2.0.37'
 end
